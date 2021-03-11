4a604deaa1871e7ffc65338548492ed6f0839953
cuscuz-itz666 <78763402+cuscuz-itz666@users.noreply.github.com>
2021-03-11 15:22:13 -0300

    Update cekvip.js

--- src/cekvip.js
+++ src/cekvip.js
@@ -1,7 +1,7 @@
 const cekvip = () => { 
 	return `           
 ──────────────────
-*Nome do bot* :  CAUSS BOT
+*Nome do bot* :  CUSCUZ-BOT
 ──────────────────
         『 *𝐕𝐈𝐏 𝐔𝐒𝐄𝐑* 』
 ──────────────────
